3334c483cbffc66e041cd4deaff56c1b203165e8
jozanza <hello@jsavary.com>
2015-09-14 12:06:29 -0400

    updated README.md

--- README.md
+++ README.md
@@ -32,41 +32,67 @@ Installation
 Getting Started
 ---------------
 
-```js
-import * as powerglove from 'powerglove';
+Start by importing the module:
 
-// Let's try piping a value through a series of functions
+```js
+import * as powerglove from 'powerglove'
+```
+
+Let's try piping a value through a series of functions.
+
+```
 void async () => {
   const result = await powerglove.pipe(
     n => 2,
     n => n + 4,
     n => n * 7
-  );
-  console.log(result);
-  // -> 42
-}();
-
-// We can do the same with a series of async functions
-void async () => {
-  const result = await powerglove.pipe(
-    async (n) => {
-      await sleep(100)
-      return 2
-    },
-    async (n) => {
-      await sleep(100)
-      return n + 4
-    },
-    async (n) => {
-      await sleep(100)
-      return n * 7
-    },
-  );
-  // ...and after ~300ms,
-  console.log(result);
+  )
+  console.log(result)
   // -> 42
 }();
 ```
+
+Okay, nothing special. But we have complete interop between async and async functions. Check it out:
+
+```
+void async () => {
+  const result = await hello('world');
+  console.log(result)
+  // -> 'HELLO, WORLD!'
+}()
+
+async function hello(name) {
+  return await powerglove.pipe(
+    name,
+    greet,
+    uppercase,
+    exclaim
+  );
+}
+
+function greet(...args) {
+  return greet(...args)
+}
+
+async function uppercase(...args) {
+  await sleep(100)
+  return uppercase(...args)
+}
+
+async function exclaim(...args) {
+  await sleep(100)
+  return exclaim(...args)
+}
+
+async function sleep(ms=0) {
+  return new Promise(fulfill => {
+    setTimeout(fulfill, ms)
+  })
+}
+
+```
+
+
 
 Utils
 -----
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,16 +7,30 @@ describe('Utils', () => {
 
   describe('pipe(value[, ...tasks])', () => {
 
-    it('synchronous series', async () => {
+    it('should iterate a synchronous series', async () => {
+
       let result = await powerglove.pipe(
         n => 2,
         n => n + 4,
         n => n * 7
       );
       expect(result).to.equal(42);
-    })
 
-    it('async series', async () => {
+      async function hello(name) {
+        return await powerglove.pipe(
+          name,
+          greet,
+          uppercase,
+          exclaim
+        );
+      }
+      let canonicalPhrase = await hello('world')
+      expect(canonicalPhrase).to.equal('HELLO, WORLD!');
+
+    });
+
+    it('should iterate an async series', async () => {
+
       let result = await powerglove.pipe(
         async (n) => {
           await sleep(100)
@@ -31,7 +45,19 @@ describe('Utils', () => {
           return n * 7
         },
       );
-      expect(result).to.equal(42)
+      expect(result).to.equal(42);
+
+      async function hello(name) {
+        return await powerglove.pipe(
+          name,
+          greetAsync,
+          uppercaseAsync,
+          exclaimAsync
+        );
+      }
+      let canonicalPhrase = await hello('world')
+      expect(canonicalPhrase).to.equal('HELLO, WORLD!');
+
     });
 
   });
@@ -43,4 +69,25 @@ async function sleep(ms=0) {
   return new Promise(fulfill => {
     setTimeout(fulfill, ms)
   })
+}
+function greet(x) {
+  return `Hello, ${x}`
+}
+function uppercase(x) {
+  return x.toUpperCase()
+}
+function exclaim(x) {
+  return `${x}!`
+}
+async function greetAsync(...args) {
+  await sleep(100)
+  return greet(...args)
+}
+async function uppercaseAsync(...args) {
+  await sleep(100)
+  return uppercase(...args)
+}
+async function exclaimAsync(...args) {
+  await sleep(100)
+  return exclaim(...args)
 }
